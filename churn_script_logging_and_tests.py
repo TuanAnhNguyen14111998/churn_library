07de08f8360e560f7e38f5b7965f2eda082be9bf
tuananh <tuananh14111998@gmail.com>
2022-05-27 15:50:05 +0700

    Upload code for churn library project

---
+++ churn_script_logging_and_tests.py
@@ -0,0 +1,221 @@
+'''
+Unit test for churn libary module
+Author : Nguyen Tuan Anh
+Date : 27 May 2022
+'''
+import os
+import logging
+from math import ceil
+
+import churn_library as cls
+
+
+logging.basicConfig(
+    filename='./logs/churn_library.log',
+    level=logging.INFO,
+    filemode='w',
+    format='%(name)s - %(levelname)s - %(message)s')
+
+
+def test_import():
+    '''
+    test data import - this example is completed for you to assist with the other test functions
+    '''
+    try:
+        dataframe = cls.import_data("./data/bank_data.csv")
+        logging.info("Testing import_data: SUCCESS")
+    except FileNotFoundError as err:
+        logging.error("Testing import_eda: The file wasn't found")
+        raise err
+
+    try:
+        assert dataframe.shape[0] > 0
+        assert dataframe.shape[1] > 0
+    except AssertionError as err:
+        logging.error(
+            "Testing import_data: The file doesn't appear to have rows and columns")
+        raise err
+
+
+def test_eda():
+    '''
+    test perform eda function
+    '''
+    dataframe = cls.import_data("./data/bank_data.csv")
+    try:
+        cls.perform_eda(eda_df=dataframe)
+        logging.info("Testing perform_eda: SUCCESS")
+    except KeyError as err:
+        logging.error('Perform EDA function error')
+        raise err
+
+    # Assert if `churn_distribution.png` is created
+    try:
+        assert os.path.isfile("./images/eda/churn_distribution.png") is True
+        logging.info('File %s was found', 'churn_distribution.png')
+    except AssertionError as err:
+        logging.error('File %s not found', 'churn_distribution.png')
+        raise err
+
+    # Assert if `customer_age_distribution.png` is created
+    try:
+        assert os.path.isfile(
+            "./images/eda/customer_age_distribution.png") is True
+        logging.info('File %s was found', 'customer_age_distribution.png')
+    except AssertionError as err:
+        logging.error('File %s not found', 'customer_age_distribution.png')
+        raise err
+
+    # Assert if `marital_status_distribution.png` is created
+    try:
+        assert os.path.isfile(
+            "./images/eda/marital_status_distribution.png") is True
+        logging.info('File %s was found', 'marital_status_distribution.png')
+    except AssertionError as err:
+        logging.error('File %s not found', 'marital_status_distribution.png')
+        raise err
+
+    # Assert if `total_transaction_distribution.png` is created
+    try:
+        assert os.path.isfile(
+            "./images/eda/total_transaction_distribution.png") is True
+        logging.info('File %s was found', 'total_transaction_distribution.png')
+    except AssertionError as err:
+        logging.error(
+            'File %s not found',
+            'total_transaction_distribution.png')
+        raise err
+
+    # Assert if `heatmap.png` is created
+    try:
+        assert os.path.isfile("./images/eda/heatmap.png") is True
+        logging.info('File %s was found', 'heatmap.png')
+    except AssertionError as err:
+        logging.error('File %s not found', 'heatmap.png')
+        raise err
+
+
+def test_encoder_helper():
+    '''
+    test encoder helper
+    '''
+    # Load DataFrame
+    dataframe = cls.import_data("./data/bank_data.csv")
+
+    # Categorical Features
+    cat_columns = [
+        'Gender', 'Education_Level',
+        'Marital_Status', 'Income_Category',
+        'Card_Category']
+    try:
+        _ = cls.encoder_helper(
+            dataframe=dataframe,
+            category_lst=cat_columns,
+            response="Churn"
+        )
+        logging.info("Testing encoder_helper: SUCCESS")
+    except AssertionError as err:
+        logging.error("Testing encoder_helper: ERROR")
+
+        raise err
+
+
+def test_perform_feature_engineering():
+    '''
+    test perform_feature_engineering
+    '''
+# Load the DataFrame
+    dataframe = cls.import_data("./data/bank_data.csv")
+    try:
+        (_, x_test, _, _) = cls.perform_feature_engineering(
+            dataframe=dataframe,
+            response='Churn'
+        )
+        logging.info("Testing perform_feature_engineering: SUCCESS")
+    except KeyError as err:
+        logging.error(
+            'The `Churn` column is not present in the DataFrame: ERROR')
+        raise err
+
+    try:
+        # x_test size should be 30% of `data_frame`
+        assert (
+            x_test.shape[0] == ceil(
+                dataframe.shape[0] *
+                0.3)) is True   # pylint: disable=E1101
+        logging.info(
+            'Testing perform_feature_engineering. DataFrame sizes are consistent: SUCCESS')
+    except AssertionError as err:
+        logging.error(
+            'Testing perform_feature_engineering. DataFrame sizes are not correct: ERROR')
+        raise err
+
+
+def test_train_models():
+    '''
+    test train_models
+    '''
+    # Load the DataFrame
+    dataframe = cls.import_data("./data/bank_data.csv")
+
+    # Feature engineering
+    (x_train, x_test, y_train, y_test) = cls.perform_feature_engineering(
+        dataframe=dataframe,
+        response='Churn')
+
+    # Assert if `logistic_model.pkl` file is present
+    try:
+        cls.train_models(x_train, x_test, y_train, y_test)
+        logging.info('Testing train_models: SUCCESS')
+    except AssertionError as err:
+        logging.error('Testing train_models: ERROR')
+        raise err
+
+    # Assert if `rfc_model.pkl` file is present
+    try:
+        assert os.path.isfile("./models/rfc_model.pkl") is True
+        logging.info('File %s was found', 'rfc_model.pkl')
+    except AssertionError as err:
+        logging.error('Not such file on disk')
+        raise err
+
+    # Assert if `roc_curve_result.png` file is present
+    try:
+        assert os.path.isfile('./images/results/roc_curve_result.png') is True
+        logging.info('File %s was found', 'roc_curve_result.png')
+    except AssertionError as err:
+        logging.error('Not such file on disk')
+        raise err
+
+    # Assert if `rfc_results.png` file is present
+    try:
+        assert os.path.isfile('./images/results/rf_results.png') is True
+        logging.info('File %s was found', 'rf_results.png')
+    except AssertionError as err:
+        logging.error('Not such file on disk')
+        raise err
+
+    # Assert if `logistic_results.png` file is present
+    try:
+        assert os.path.isfile('./images/results/logistic_results.png') is True
+        logging.info('File %s was found', 'logistic_results.png')
+    except AssertionError as err:
+        logging.error('Not such file on disk')
+        raise err
+
+    # Assert if `feature_importances.png` file is present
+    try:
+        assert os.path.isfile(
+            './images/results/feature_importances.png') is True
+        logging.info('File %s was found', 'feature_importances.png')
+    except AssertionError as err:
+        logging.error('Not such file on disk')
+        raise err
+
+
+if __name__ == "__main__":
+    test_import()
+    test_eda()
+    test_encoder_helper()
+    test_perform_feature_engineering()
+    test_train_models()
